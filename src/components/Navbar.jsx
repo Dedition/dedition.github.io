2433c313bf4c6ca14872070881ff0952196bc7c1
Leo Lad <87998262+Dedition@users.noreply.github.com>
2023-03-08 14:17:24 -0500

    Fixed logo issue,  added postprocessor file and allowed vite to work with tailwind. Added navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 import { styles } from '../styles';
 import { navLinks } from '../constants';
-import { leologotwo, menu, close } from '../assets';
+import { leologopng, menu, close } from '../assets';
 
 
 const Navbar = () => {
@@ -21,9 +21,22 @@ const Navbar = () => {
             setActive("");
             window.scrollTo(0, 0);
           }}>
-          <img src={leologotwo} alt="leo-logo" className="w-9 h-9 object-cover" />
-
+          <img src={leologopng} alt="leo-logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">Leo <span className="sm:block hidden">| Software Engineer</span></p>
         </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={
+                `${active === link.title ? "text-white"
+                  : "text-secondary"}
+              hover:text-white text-[18px] font-medium cursor-pointer`}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   )
