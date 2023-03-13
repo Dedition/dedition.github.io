19346c683ec7f9637baadb5fb80ed0c9e16d3f0b
Leo Lad <87998262+Dedition@users.noreply.github.com>
2023-03-13 06:20:27 -0400

    Created skeleton feedback

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -7,7 +7,26 @@ import { testimonials } from "../constants";
 
 const Feedbacks = () => {
   return (
-    <></>
+    <div className="mt-12 bg-black-100 rounded-[20px]">
+      <div className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}>
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>
+            What others say
+          </p>
+          <h2 className={styles.sectionHeadText}>Testimonials</h2>
+        </motion.div>
+      </div>
+      <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7`}>
+        {testimonials.map((test, index) => (
+          <FeedbackCard
+            key={test.name}
+            index={index}
+            {...test}
+          />
+        ))}
+
+      </div>
+    </div>
   )
 }
 
