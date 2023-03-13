793483e06a62abf798d35844c201ffe172f3c8e7
Leo Lad <87998262+Dedition@users.noreply.github.com>
2023-03-12 21:37:46 -0400

    Added appropriate svg logo

--- src/assets/index.js
+++ src/assets/index.js
@@ -27,6 +27,7 @@ import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
+import abm from "./company/abm.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -63,4 +64,5 @@ export {
   carrent,
   jobit,
   tripguide,
+  abm,
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -16,6 +16,7 @@ import {
     figma,
     docker,
     meta,
+    abm,
     starbucks,
     tesla,
     shopify,
@@ -118,8 +119,8 @@ const experiences = [
     {
         title: "React.js Developer",
         company_name: "Starbucks",
-        icon: starbucks,
-        iconBg: "#383E56",
+        icon: abm,
+        iconBg: "#E6DEDD",
         date: "March 2020 - April 2021",
         points: [
             "Developing and maintaining web applications using React.js and other related technologies.",
@@ -140,32 +141,32 @@ const experiences = [
             "Implementing responsive design and ensuring cross-browser compatibility.",
             "Participating in code reviews and providing constructive feedback to other developers.",
         ],
-    },
-    {
-        title: "Web Developer",
-        company_name: "Shopify",
-        icon: shopify,
-        iconBg: "#383E56",
-        date: "Jan 2022 - Jan 2023",
-        points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-        ],
-    },
-    {
-        title: "Full stack Developer",
-        company_name: "Meta",
-        icon: meta,
-        iconBg: "#E6DEDD",
-        date: "Jan 2023 - Present",
-        points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-        ],
+        // },
+        // {
+        //     title: "Web Developer",
+        //     company_name: "Shopify",
+        //     icon: shopify,
+        //     iconBg: "#383E56",
+        //     date: "Jan 2022 - Jan 2023",
+        //     points: [
+        //         "Developing and maintaining web applications using React.js and other related technologies.",
+        //         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        //         "Implementing responsive design and ensuring cross-browser compatibility.",
+        //         "Participating in code reviews and providing constructive feedback to other developers.",
+        //     ],
+        // },
+        // {
+        //     title: "Full stack Developer",
+        //     company_name: "Meta",
+        //     icon: meta,
+        //     iconBg: "#E6DEDD",
+        //     date: "Jan 2023 - Present",
+        //     points: [
+        //         "Developing and maintaining web applications using React.js and other related technologies.",
+        //         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        //         "Implementing responsive design and ensuring cross-browser compatibility.",
+        //         "Participating in code reviews and providing constructive feedback to other developers.",
+        //     ],
     },
 ];
 
