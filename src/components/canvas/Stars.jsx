473de090d1693dab8b2bd9afd5ae764a1ca7e137
Leo Lad <87998262+Dedition@users.noreply.github.com>
2023-03-13 07:55:24 -0400

    Stars are rendered

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -3,9 +3,22 @@ import { Canvas, useFrame } from '@react-three/fiber';
 import { Points, PointMaterial, Preload } from '@react-three/drei';
 import * as random from 'maath/random/dist/maath-random.esm';
 
-const Stars = () => {
+const Stars = (props) => {
+  const ref = useRef();
+  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
+
   return (
-    <div>Stars</div>
+    <group>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+        <PointMaterial
+          transparent
+          size={0.002}
+          color="#f272c8"
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
   )
 }
 
@@ -23,4 +36,4 @@ const StarsCanvas = () => {
   )
 }
 
-export default Stars
+export default StarsCanvas;
