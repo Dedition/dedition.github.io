8e30381449b154945ae839a7139a4f272ce194c7
Leo Lad <87998262+Dedition@users.noreply.github.com>
2023-03-12 21:24:22 -0400

    Got experience to display

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -9,24 +9,48 @@ import { SectionWrapper } from '../hoc';
 import { textVariant } from '../utils/motion';
 
 const ExperienceCard = ({ experience }) => {
-  <VerticalTimelineElement
-    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-    contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-    date={experience.date}
-    iconStyle={{ background: experience.iconBg }}
-    icon={
+  return (
+    <VerticalTimelineElement
+      contentStyle={{
+        background: 'rgb(33, 150, 243)',
+        color: '#fff'
+      }}
+      contentArrowStyle={{
+        borderRight: '7px solid  rgb(33, 150, 243)'
+      }}
+      date={experience.date}
+      iconStyle={{ background: experience.iconBg }}
+      icon={
+        <div className="flex justify-center items-center w-full h-full">
+          <img
+            src={experience.icon}
+            alt={experience.company_name}
+            className="w-[60%] h-[60%] object-contain"
+          />
+        </div>
+      }
+    >
       <div>
-        <img src={experience.icon} alt={experience.company_name}
-          className="w-[60%] h-[60%] object-contain"
-        />
+        <h3 className="text-white text-[24px] font-bold">{experience.title}</h3>
+        <p className="text-secondary text-[16px] font-semibold"
+          style={{ margin: 0 }}>
+          {experience.company_name}
+        </p>
       </div>
-    }
-  >
-    <div>
 
-    </div>
+      <ul className="mt-5 list-disc ml-5 space-y-2">
+        {experience.points.map((point, index) => (
+          <li
+            key={`experience-point-${index}`}
+            className="text-white-100 text-[14px] pl-1 tracking-wider"
+          >
+            {point}
+          </li>
+        ))}
+      </ul>
 
-  </VerticalTimelineElement>
+    </VerticalTimelineElement>
+  )
 }
 
 const Experience = () => {
